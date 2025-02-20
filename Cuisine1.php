e4b8fd7ed76a99e904cb50b2471572fb00cf2aaa
Mahamadou Amadou Habou Gremah <mahamadou8877@gmail.com>
2025-02-20 22:51:43 +0100

    Improvement of tables and creation of 3 functions including displayAuthors

--- Cuisine1.php
+++ Cuisine1.php
@@ -1,60 +1,99 @@
+<?php
+
+$users = [
+    [
+        'full_name' => 'Mickaël Andrieu',
+        'email' => 'mickael.andrieu@exemple.com',
+        'age' => 34,
+    ],
+    [
+        'full_name' => 'Mathieu Nebra',
+        'email' => 'mathieu.nebra@exemple.com',
+        'age' => 34,
+    ],
+    [
+        'full_name' => 'Laurène Castor',
+        'email' => 'laurene.castor@exemple.com',
+        'age' => 28,
+    ],
+];
+
+$recipes = [
+    [
+        'title' => 'Cassoulet',
+        'recipe' => 'Etape 1 : des flageolets !',
+        'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => true,
+    ],
+    [
+        'title' => 'Couscous',
+        'recipe' => 'Etape 1 : de la semoule',
+        'author' => 'mickael.andrieu@exemple.com',
+        'is_enabled' => false,
+    ],
+    [
+        'title' => 'Escalope milanaise',
+        'recipe' => 'Etape 1 : prenez une belle escalope',
+        'author' => 'mathieu.nebra@exemple.com',
+        'is_enabled' => true,
+    ],
+];
+
+function displayAuthor(string $authorEmail, array $users): string
+{
+    foreach ($users as $user) {
+        if ($authorEmail === $user['email']) {
+            return $user['full_name'] . '(' . $user['age'] . ' ans)';
+        }
+    }
+    return 'Author not found';
+}
+
+function isValidRecipe(array $recipe): bool
+{
+    if (array_key_exists('is_enabled', $recipe)) {
+        $isEnabled = $recipe['is_enabled'];
+    } else {
+        $isEnabled = false;
+    }
+
+    return $isEnabled;
+}
+
+function getRecipes(array $recipes): array
+{
+    $valid_recipes = [];
+
+    foreach ($recipes as $recipe) {
+        if (isValidRecipe($recipe)) {
+            $valid_recipes[] = $recipe;
+        }
+    }
+
+    return $valid_recipes;
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Recettes</title>
+    <title>Recettes de cuisine</title>
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+    >
 </head>
 <body>
-    <?php
-        $users = [
-            [
-                'full_name' => 'Mickaël Andrieu',
-                'email' => 'mickael.andrieu@exemple.com',
-                'age' => 34,
-            ],
-            [
-                'full_name' => 'Mathieu Nebra',
-                'email' => 'mathieu.nebra@exemple.com',
-                'age' => 34,
-            ],
-            [
-                'full_name' => 'Laurène Castor',
-                'email' => 'laurene.castor@exemple.com',
-                'age' => 28,
-            ],
-        ];
-        $lines1 = 3; // nombre d'utilisateurs dans le tableau
-        $counter1 = 0;
-        while ($counter1 < $lines1) {
-            echo $users[$counter1]['full_name'] . ' ' . $users[$counter1]['email'] . '<br />';
-            $counter1++;
-        }
-        $recipes = [
-            [
-                'title' => 'Cassoulet',
-                'recipe' => 'Etape 1 : des flageolets !',
-                'author' => 'mickael.andrieu@exemple.com',
-                'is_enabled' => true,
-            ],
-            [
-                'title' => 'Couscous',
-                'recipe' => 'Etape 1 : de la semoule',
-                'author' => 'mickael.andrieu@exemple.com',
-                'is_enabled' => false,
-            ],
-            [
-                'title' => 'Escalope milanaise',
-                'recipe' => 'Etape 1 : prenez une belle escalope',
-                'author' => 'mathieu.nebra@exemple.com',
-                'is_enabled' => true,
-            ],
-        ];
-        
-        foreach ($recipes as $recipe) {
-            echo $recipe['title'] . ' ' . $recipe['author'] . '<br />';
-        }
-    ?>
-    
+<div class="container">
+    <h1>Liste des recettes de cuisine</h1>
+
+    <?php foreach (getRecipes($recipes) as $recipe) : ?>
+        <article>
+            <h3><?php echo $recipe['title']; ?></h3>
+            <div><?php echo $recipe['recipe']; ?></div>
+            <i><?php echo displayAuthor($recipe['author'], $users); ?></i>
+        </article>
+    <?php endforeach ?>
+</div>
 </body>
 </html>
